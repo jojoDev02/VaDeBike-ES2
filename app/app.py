7a09bb37aba857ea5ed52ef93f3fa9a85e49026f
Rômulo Pastore <88741244+Romumoon@users.noreply.github.com>
2023-10-21 17:09:39 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello, World!</h1>',200
+    return '<h1>Hello, World!</h1>', 200
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
