9792d9f7a605fa8a0f2349b29fd94d3d48c7936f
Maria Jordana <jordana-cavalcante@outlook.com>
2023-10-21 20:52:56 -0300

    testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,5 +4,5 @@ from app.app import index
 def test_index():
     response = index()
     content, status_code = response
-    assert '<h1>Hello, World!</h1>' in content
+    assert '<h1>Jojo do DevOps</h1>' in content
     assert status_code == 200
